c128a8b9774d42f86e7149ca283c7cef02873085
viniciusdev772 <viniciusdev37@gmail.com>
2023-06-13 19:56:04 -0300

    update

--- src/middlewars.js
+++ src/middlewars.js
@@ -11,7 +11,7 @@ const fs = require("fs");
 const crypto = require('crypto');
 const axios = require('axios');
 
-const  numero = "" ;
+var numero = "" ;
 
 async function shortenUrl(longUrl) {
   try {
